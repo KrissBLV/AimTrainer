4b5515239e3fec6dee92708faa42f0511fd743c3
kristofers birks <kristofers.birks@gmail.com>
2020-06-05 08:40:31 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace AimTrainer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.QuitButton = new System.Windows.Forms.Button();
+            this.ClickButton = new System.Windows.Forms.Button();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.PauseButton = new System.Windows.Forms.Button();
+            this.RandomTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(400, 115);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(200, 80);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start Game";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // QuitButton
+            // 
+            this.QuitButton.Location = new System.Drawing.Point(400, 221);
+            this.QuitButton.Name = "QuitButton";
+            this.QuitButton.Size = new System.Drawing.Size(200, 80);
+            this.QuitButton.TabIndex = 1;
+            this.QuitButton.Text = "Quit Game";
+            this.QuitButton.UseVisualStyleBackColor = true;
+            this.QuitButton.Click += new System.EventHandler(this.QuitButton_Click);
+            // 
+            // ClickButton
+            // 
+            this.ClickButton.Location = new System.Drawing.Point(237, 159);
+            this.ClickButton.Name = "ClickButton";
+            this.ClickButton.Size = new System.Drawing.Size(75, 74);
+            this.ClickButton.TabIndex = 2;
+            this.ClickButton.UseVisualStyleBackColor = true;
+            this.ClickButton.Visible = false;
+            this.ClickButton.Click += new System.EventHandler(this.ClickButton_Click);
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.Location = new System.Drawing.Point(480, 9);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(44, 13);
+            this.ScoreLabel.TabIndex = 3;
+            this.ScoreLabel.Text = "Score 0";
+            this.ScoreLabel.Visible = false;
+            // 
+            // PauseButton
+            // 
+            this.PauseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.PauseButton.Location = new System.Drawing.Point(12, 4);
+            this.PauseButton.Name = "PauseButton";
+            this.PauseButton.Size = new System.Drawing.Size(30, 27);
+            this.PauseButton.TabIndex = 4;
+            this.PauseButton.Text = "| |";
+            this.PauseButton.UseVisualStyleBackColor = true;
+            this.PauseButton.Visible = false;
+            this.PauseButton.Click += new System.EventHandler(this.PauseButton_Click);
+            // 
+            // RandomTimer
+            // 
+            this.RandomTimer.Tick += new System.EventHandler(this.StartButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.PauseButton);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.ClickButton);
+            this.Controls.Add(this.QuitButton);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button QuitButton;
+        private System.Windows.Forms.Button ClickButton;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Button PauseButton;
+        private System.Windows.Forms.Timer RandomTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AimTrainer
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        int score = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            ClickButton.Visible = true;
+            ScoreLabel.Visible = true;
+            StartButton.Visible = false;
+            QuitButton.Visible = false;
+            PauseButton.Visible = true;
+        }
+
+        private void ClickButton_Click(object sender, EventArgs e)
+        {
+            ClickButton.Width = rand.Next(40, 150);
+            ClickButton.Height = ClickButton.Width;
+            ClickButtonMove();
+            score += 1;
+            UpdateScoreLabel();
+            ClickButtonSize();
+        }
+
+        private void ClickButtonSize()
+        {
+            ClickButton.Width = rand.Next(40, 150);
+            ClickButton.Height = ClickButton.Width;
+        }
+
+        private void ClickButtonMove()
+        {
+            ClickButton.Top = rand.Next(40, 450);
+            ClickButton.Left = rand.Next(40, 950);
+            
+        }
+        private void UpdateScoreLabel()
+        {
+            ScoreLabel.Text = "Score "+ score;
+        }
+
+        private void QuitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void PauseButton_Click(object sender, EventArgs e)
+        {
+            QuitButton.Visible = true;
+        }
+        
+        
+
+        
+        
+    }
+}
